68644132c13e30d8d5c98c2137f1aded4dac53aa
cheef <cheef@mail.ua>
2018-09-05 23:27:45 +0700

    Домашка 1 по Asp.Net

---
+++ Homework 1/AspProjekt/AspProjekt/Models/EmployeeView.cs
@@ -0,0 +1,19 @@
+﻿
+namespace AspProjekt.Models
+{
+    public class EmployeeView
+    {
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public string Patronymic { get; set; }
+        public int Age { get; set; }
+
+        public string WorkRole { get; set; }
+        public string Birth { get; set; }
+        public string Departament { get; set; }
+        public string WorkingSince { get; set; }
+
+    }
+}
