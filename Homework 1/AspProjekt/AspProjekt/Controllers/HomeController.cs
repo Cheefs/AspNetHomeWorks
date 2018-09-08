afd2d78c354ef47d10f19377d6004e6d6a6ee375
cheef <cheef@mail.ua>
2018-09-09 01:02:33 +0700

    Работа над дз2

--- Homework 1/AspProjekt/AspProjekt/Controllers/HomeController.cs
+++ Homework 1/AspProjekt/AspProjekt/Controllers/HomeController.cs
@@ -1,75 +1,42 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using AspProjekt.Models;
 
 namespace AspProjekt.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly List<EmployeeView> _employees = new List<EmployeeView>
-        {
-            new EmployeeView
-            {
-                Id = 1,
-                FirstName = "Иван",
-                SurName = "Иванов",
-                Patronymic = "Иванович",
-                Age = 22,
-                Birth="18.02.1996",
-                Departament="GameDev",
-                WorkRole="Unity Developer",
-                WorkingSince="10.10.2016"
-            },
-            new EmployeeView
-            {
-                Id = 2,
-                FirstName = "Владислав",
-                SurName = "Петров",
-                Patronymic = "Иванович",
-                Age = 45,
-                Birth="18.02.1979",
-                Departament="Web",
-                WorkRole="Senior Developer",
-                WorkingSince="10.10.2005"
-            },
-            new EmployeeView
-            {
-                Id = 3,
-                FirstName = "Николай",
-                SurName = "Николаев",
-                Patronymic = "николаевич",
-                Age = 32,
-                Birth="18.02.1989",
-                Departament="Web",
-                WorkRole="FrontEnd developer",
-                WorkingSince="10.10.2014"
-            },
-            new EmployeeView
-            {
-                Id = 4,
-                FirstName = "Петр",
-                SurName = "Петров",
-                Patronymic = "Петрович",
-                Age = 35,
-                Birth="18.02.1986",
-                Departament="GameDev",
-                WorkRole="Senior Unity Developer",
-                WorkingSince="10.10.2010"
-            }
-        };
-
-        public IActionResult Details(int id)
-        {
-            var employe = _employees.FirstOrDefault(t => t.Id.Equals(id));
-            if (ReferenceEquals(employe, null))
-                return NotFound();
-            else return View(employe);
-        }
-
         public IActionResult Index()
         {
-            return View(_employees);
+            return View();
         }
+
+        public IActionResult Shop()
+        {
+            return View();
+        }
+
+        public IActionResult ProductDetails()
+        {
+            return View();
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
     }
 }
