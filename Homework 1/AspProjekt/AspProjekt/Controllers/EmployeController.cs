b4d84c79f993f76e3f0eca5fdebf9ac394768d8d
cheef <cheef@mail.ua>
2018-09-09 18:00:51 +0700

    Реализация редактирования, удаления сотрудников (отдеьноое задание, доступно при использовании роутов), и окончание внедрения штмл верстки в код

--- Homework 1/AspProjekt/AspProjekt/Controllers/EmployeController.cs
+++ Homework 1/AspProjekt/AspProjekt/Controllers/EmployeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using AspProjekt.Infrastructure.Interfaces;
+using AspProjekt.Models;
 
 namespace AspProjekt.Controllers
 {
@@ -16,7 +17,8 @@ namespace AspProjekt.Controllers
         public IActionResult Index()
         {
             return View(_employeesData.GetAll());
-        }
+        }
+
         [Route("{id}")]
         public IActionResult Details(int id)
         {
@@ -32,6 +34,49 @@ namespace AspProjekt.Controllers
             _employeesData.Delete(id);
             return RedirectToAction(nameof(Index));
         }
-    }
-    
+
+        [Route("edit/{id?}")]
+        public IActionResult Edit(int? id)
+        {
+            EmployeeView model;
+            if (id.HasValue)
+            {
+                model = _employeesData.GetById(id.Value);
+                if (ReferenceEquals(model, null))
+                    return NotFound();
+            }
+            else
+            {
+                model = new EmployeeView();
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [Route("edit/{id?}")]
+        public IActionResult Edit(EmployeeView model)
+        {
+            if (model.Id > 0)
+            {
+                var employe = _employeesData.GetById(model.Id);
+                if (ReferenceEquals(employe, null))
+                    return NotFound();
+
+                employe.FirstName = model.FirstName;
+                employe.SurName = model.SurName;
+                employe.Age = model.Age;
+                employe.Patronymic = model.Patronymic;
+                employe.Birth = employe.Birth;
+                employe.Departament = employe.Departament;
+                employe.WorkingSince = employe.WorkingSince;
+                employe.WorkRole = employe.WorkRole;
+            }
+            else
+            {
+                _employeesData.AddNew(model);
+            }
+            _employeesData.Edit();
+            return RedirectToAction(nameof(Index));
+        }
+    }    
 }
--- Homework 1/AspProjekt/AspProjekt/Infrastructure/EmployeData.cs
+++ Homework 1/AspProjekt/AspProjekt/Infrastructure/EmployeData.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using AspProjekt.Models;
 using AspProjekt.Infrastructure.Interfaces;
 
@@ -13,58 +11,88 @@ namespace AspProjekt.Infrastructure
 
         public EmployeesData()
         {
-            _employees = new List<EmployeeView>
-            {
-                new EmployeeView
-            {
-                Id = 1,
-                FirstName = "Иван",
-                SurName = "Иванов",
-                Patronymic = "Иванович",
-                Age = 22,
-                Birth="18.02.1996",
-                Departament="GameDev",
-                WorkRole="Unity Developer",
-                WorkingSince="10.10.2016"
-            },
-                new EmployeeView
-            {
-                Id = 2,
-                FirstName = "Владислав",
-                SurName = "Петров",
-                Patronymic = "Иванович",
-                Age = 45,
-                Birth="18.02.1979",
-                Departament="Web",
-                WorkRole="Senior Developer",
-                WorkingSince="10.10.2005"
-            },
-                new EmployeeView
-            {
-                Id = 3,
-                FirstName = "Николай",
-                SurName = "Николаев",
-                Patronymic = "николаевич",
-                Age = 32,
-                Birth="18.02.1989",
-                Departament="Web",
-                WorkRole="FrontEnd developer",
-                WorkingSince="10.10.2014"
-            },
-                new EmployeeView
-            {
-                Id = 4,
-                FirstName = "Петр",
-                SurName = "Петров",
-                Patronymic = "Петрович",
-                Age = 35,
-                Birth="18.02.1986",
-                Departament="GameDev",
-                WorkRole="Senior Unity Developer",
-                WorkingSince="10.10.2010"
-            }
-            };
+            _employees = new List<EmployeeView>();
+
+            _employees.Add(NewEmploye(1, "Иван", "Иванов", "Иванович", 22, "18.02.1996", "GameDev", "Unity Developer", "10.10.2016"));
+            _employees.Add(NewEmploye(2, "Владиславcкий", "Петр", "Иванович", 45, "18.02.1979", "Web", "Senior Developer", "10.10.2005"));
+            _employees.Add(NewEmploye(3, "Николай", "Николаев", "николаевич", 32, "18.02.1989", "Web", "FrontEnd developer", "10.10.2014"));
+            _employees.Add(NewEmploye(4, "Петр", "Петров", "Петрович", 35, "18.02.1986", "GameDev", "Senior Unity Developer", "10.10.2010"));
+
+            #region old
+            //_employees = new List<EmployeeView>
+            //{
+            //    new EmployeeView
+            //{
+            //    Id = 1,
+            //    FirstName = "Иван",
+            //    SurName = "Иванов",
+            //    Patronymic = "Иванович",
+            //    Age = 22,
+            //    Birth ="18.02.1996",
+            //    Departament="GameDev",
+            //    WorkRole="Unity Developer",
+            //    WorkingSince="10.10.2016"
+            //},
+            //    new EmployeeView
+            //{
+            //    Id = 2,
+            //    FirstName = "Владислав",
+            //    SurName = "Петров",
+            //    Patronymic = "Иванович",
+            //    Age = 45,
+            //    Birth="18.02.1979",
+            //    Departament="Web",
+            //    WorkRole="Senior Developer",
+            //    WorkingSince="10.10.2005"
+            //},
+            //    new EmployeeView
+            //{
+            //    Id = 3,
+            //    FirstName = "Николай",
+            //    SurName = "Николаев",
+            //    Patronymic = "николаевич",
+            //    Age = 32,
+            //    Birth="18.02.1989",
+            //    Departament="Web",
+            //    WorkRole="FrontEnd developer",
+            //    WorkingSince="10.10.2014"
+            //},
+            //    new EmployeeView
+            //{
+            //    Id = 4,
+            //    FirstName = "Петр",
+            //    SurName = "Петров",
+            //    Patronymic = "Петрович",
+            //    Age = 35,
+            //    Birth="18.02.1986",
+            //    Departament="GameDev",
+            //    WorkRole="Senior Unity Developer",
+            //    WorkingSince="10.10.2010"
+            //}
+            //};
+            #endregion
+
         }
+
+        public EmployeeView NewEmploye(int id, string firstName, string surName, string partonymic, int age, string birth, string departament,
+            string workRole, string workingSince)
+        {
+           var empl = new EmployeeView
+           {
+                Id = id,
+                FirstName = firstName,
+                SurName = surName,
+                Patronymic = partonymic,
+                Age = age,
+                Birth = birth,
+                Departament = departament,
+                WorkRole = workRole,
+                WorkingSince = workingSince
+           };
+
+            return empl;
+        }
+
         public IEnumerable<EmployeeView> GetAll()
         {
             return _employees;
